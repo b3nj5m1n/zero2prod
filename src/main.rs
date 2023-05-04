5db1bbb033629185d25b97590c5e96feb361cbaa
b3nj5m1n <b3nj4m1n@gmx.net>
2023-05-04 18:14:37 +0200

    Implement configuration system

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::net::TcpListener;
 
+use zero2prod::configuration::get_configuration;
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:3000")?;
+    let config = get_configuration().expect("Failed to read config");
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", config.application_port))?;
     zero2prod::startup::run(listener)
         .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?
         .await
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,6 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
-
-use anyhow::Result;
+use zero2prod::configuration::get_configuration;
 
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
@@ -28,6 +28,13 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_200_for_valid_form_data() {
     let addr = spawn_app();
+    let config = get_configuration().expect("Failed to read configuration");
+    let conn_string = config.database.connection_string();
+
+    let mut connection = PgConnection::connect(&conn_string)
+        .await
+        .expect("Failed to connect to postgres");
+
     let client = reqwest::Client::new();
 
     let body = "name=benjamin&email=b3nj4m1n%40gmx.net";
@@ -39,7 +46,15 @@ async fn subscribe_returns_200_for_valid_form_data() {
         .await
         .expect("Failed to send request");
 
-    assert_eq!(200, response.status().as_u16())
+    assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.email, "b3nj4m1n@gmx.net");
+    assert_eq!(saved.name, "benjamin");
 }
 
 #[tokio::test]
